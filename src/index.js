f500d44686bf4be51c07bb1d730491b7f5d229e7
devkhalil1337@gmail.com <devkhalil1337@gmail.com>
2020-12-03 21:25:55 +0500

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import DemoApp from './main';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <DemoApp />
   </React.StrictMode>,
   document.getElementById('root')
 );
